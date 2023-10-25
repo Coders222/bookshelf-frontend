3164d738e73c54bb4f5dd7132c79bd7fea2d8b4b
Orion Chen <orion.chen@hotmail.com>
2023-10-25 18:24:07 -0400

    added more stuff

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,15 @@
 // https://uiverse.io/emmanuelh-dev/plastic-rattlesnake-49
+import request from "./fetch"
+
 export default function SearchBar(props){
+
+    function setData() {
+        props.setResult(props.string)
+    }
+    async function search(callback){
+        request('/search.json?title=', 'get', {search: props.string})
+        callback()
+    }
     return (
         <div class="flex items-center justify-center p-5">
                 <div class="flex">
@@ -9,7 +19,7 @@ export default function SearchBar(props){
                     </svg>
                 </div>
                 <input type="text" value = {props.string} onChange = {(e) => props.setString(e.target.value)} class="w-full max-w-[160px] bg-white pl-2 text-base font-semibold outline-0" placeholder="Lebron" id=""/>
-                <input type="button" value="Search" class="bg-blue-500 p-2 rounded-tr-lg rounded-br-lg text-white font-semibold hover:bg-blue-800 transition-colors"/>
+                <input type="button" value="Search" onClick = {() => search(setData)} class="bg-blue-500 p-2 rounded-tr-lg rounded-br-lg text-white font-semibold hover:bg-blue-800 transition-colors"/>
                 </div>
         </div>
     )
