17a2476f052389fc43f0f880af2e4a43d06f1f7e
Orion <orion.chen@hotmail.com>
2023-10-27 21:22:01 -0400

    add register button

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -78,11 +78,20 @@ export default function Landing(){
         }
     }
     
+    // button from 
+    // https://uiverse.io/tranphattrien/modern-bird-56
     return (
         <div class='h-screen bg-[#363139]'>
             <h1 class='font-extrabold text-4xl text-center text-[#fb9575] cursor-default select-none'>
                 Bookshelf
             </h1>
+            
+            <a href = "/Register">
+                <button class="ml-24 text-3xl w-[200px] font-bold bg-black h-[70px] my-3 flex items-center justify-center rounded-xl cursor-pointer absolute overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#009b49] before:to-[rgb(105,184,141)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]">
+                    Register
+                </button>
+            </a>
+
 
             <Main bookstack = {bookstack}/>
             <Search setBooks = {setBooks} books = {books} searching = {searching} setSearching = {setSearching}/>
